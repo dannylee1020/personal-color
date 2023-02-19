bf43df1c576cf91b535c807209c87730d5ed2ab1
Daniel Lee <daniel.lee@circleci.com>
2023-02-18 18:14:52 -0800

    entrypoint'

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from "./screens/Home";
+import FirstQ from "./screens/FirstQ";
+import SecondQ from "./screens/SecondQ";
+import ThirdQ from "./screens/ThirdQ";
+import CameraVision from "./screens/CameraVision";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="FirstQ" component={FirstQ} />
+                <Stack.Screen name="SecondQ" component={SecondQ} />
+                <Stack.Screen name="ThirdQ" component={ThirdQ} />
+                <Stack.Screen name="CameraVision" component={CameraVision} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
